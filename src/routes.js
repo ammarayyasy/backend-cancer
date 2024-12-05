17c838d7f9cd8f121bf5336b0d6b6db5c5988945
ammarayyasy <ammarmojur@gmail.com>
2024-12-05 10:41:49 +0700

    update

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,26 @@
 const { predict } = require('./handler');
 
 const routes = [
-  {
-      method: 'POST',
-      path: '/predict',
-      handler: predict,
-      options: {
-          payload: {
-              allow: 'multipart/form-data',
-              multipart: true,
-              output: 'stream',
-              maxBytes: 1048576,
-          },
-      },
-  }
-  ];
-   
+    {
+        method: 'GET', // Method GET untuk route /
+        path: '/',
+        handler: (request, h) => {
+            return 'Welcome'; // Mengembalikan teks "Welcome"
+        },
+    },
+    {
+        method: 'POST',
+        path: '/predict',
+        handler: predict,
+        options: {
+            payload: {
+                allow: 'multipart/form-data',
+                multipart: true,
+                output: 'stream',
+                maxBytes: 1048576,
+            },
+        },
+    },
+];
+
 module.exports = routes;
